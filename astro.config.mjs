cb8777e4d9ec9546a5a002878c6fe494ec7ca032
intercepted16 <162676516+intercepted16@users.noreply.github.com>
2026-08-24 18:13:29 +1000

    feat: modify borders and color scheme of code blocks
    Changed the theme to `github-light` instead of `github-dark`, and added
    a border so it's easier to read.
    
    - removed monospace font for bullets
    - made the border for images thicker and styled better

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,10 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
+  markdown: {
+    shikiConfig: {
+      theme: "github-light",
+    },
+  },
   integrations: [mdx()]
 });
