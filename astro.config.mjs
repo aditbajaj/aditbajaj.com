362be073cfb77245eb5b2cdb88ac61092c2f123c
intercepted16 <162676516+intercepted16@users.noreply.github.com>
2026-09-09 22:35:07 +1000

    fix: pin cloudflare adapter and astro versions to match

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
+  adapter: cloudflare(),
   markdown: {
     shikiConfig: {
       theme: "github-light",
