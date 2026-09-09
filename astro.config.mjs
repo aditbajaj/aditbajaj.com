631fe7d146606a4d8e9c7fbf7c03dce0a331b628
intercepted16 <162676516+intercepted16@users.noreply.github.com>
2026-09-09 22:36:57 +1000

    fix: ship a wrangler config instead of auto-adding the cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  adapter: cloudflare(),
   markdown: {
     shikiConfig: {
       theme: "github-light",
